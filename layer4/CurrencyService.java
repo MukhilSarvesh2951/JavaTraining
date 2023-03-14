15923de572bed02d2bbaf39e230edf047f29d749
MukhilSarvesh2951 <125636434+MukhilSarvesh2951@users.noreply.github.com>
2023-03-14 17:45:29 +0530

    layer4

---
+++ layer4/CurrencyService.java
@@ -0,0 +1,17 @@
+package com.java.layer4;
+
+import java.util.List;
+
+import com.java.layer2.Currency;
+
+public interface CurrencyService {
+	/*Currency findCurrencyService(int currencyId);
+	List<Currency> findAllAllCurrenciesService();
+	
+	void saveCurrencyService(Currency currency);
+	void modifyCurrencyService(Currency currency);
+	void removeCurrencyService(int currencyId);
+	*/
+	float calculateCurrencyService(String s,String t, float amt) throws CurrencyNotFoundException,SourceCurrencyNotFoundException, TargetCurrencyNotFoundException;
+	
+}
